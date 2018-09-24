cf0d1b993eb5c867dcea41cb8efc59775be27c71
lisastreeter <lisastreeter@3439677.no-reply.drupal.org>
2018-09-24 15:39:24 -0500

    Issue #2990498 by mglaman, lisastreeter: Can't be installed together with Belgrade on a regular install profile

--- src/ThemeHandler.php
+++ src/ThemeHandler.php
@@ -123,6 +123,42 @@ class ThemeHandler {
         ],
         'visibility' => [],
       ],
+      'catalog' => [
+        'plugin' => 'system_menu_block:catalog',
+        'settings' => [
+          'id' => 'catalog_footer',
+          'label' => 'Catalog',
+          'provider' => 'system',
+          'label_display' => '0',
+          'level' => 1,
+          'depth' => 0,
+        ],
+        'visibility' => [],
+      ],
+      'catalog_menu' => [
+        'plugin' => 'system_menu_block:catalog',
+        'settings' => [
+          'id' => 'catalog_menu',
+          'label' => 'Catalog menu',
+          'provider' => 'system',
+          'label_display' => '0',
+          'level' => 1,
+          'depth' => 0,
+        ],
+        'visibility' => [],
+      ],
+      'catalog_menu_navigation' => [
+        'plugin' => 'system_menu_block:catalog',
+        'settings' => [
+          'id' => 'catalog_menu_navigation',
+          'label' => 'Catalog menu navigation',
+          'provider' => 'system',
+          'label_display' => '0',
+          'level' => 1,
+          'depth' => 0,
+        ],
+        'visibility' => [],
+      ],
       'breadcrumbs' => [
         'plugin' => 'system_breadcrumb_block',
         'settings' => [
@@ -160,6 +196,14 @@ class ThemeHandler {
     $data['shopping_cart']['region'] = 'header';
     $data['shopping_cart']['weight'] = NULL;
 
+    $data['catalog']['region'] = 'footer_first';
+    $data['catalog']['weight'] = -6;
+    $data['catalog_menu']['region'] = 'sidebar_first';
+    $data['catalog_menu']['weight'] = -8;
+    $data['catalog_menu']['settings']['label_display'] = 1;
+    $data['catalog_menu']['settings']['label'] = t('Catalog');
+    unset($data['catalog_menu_navigation']);
+
     $data['breadcrumbs']['region'] = 'breadcrumb';
     $data['breadcrumbs']['weight'] = 0;
   }
@@ -180,6 +224,13 @@ class ThemeHandler {
 
     $data['shopping_cart']['region'] = 'top_navigation';
     $data['shopping_cart']['weight'] = -5;
+
+    $data['catalog']['region'] = 'footer';
+    $data['catalog']['weight'] = -6;
+    $data['catalog_menu']['region'] = 'header';
+    $data['catalog_menu']['weight'] = -8;
+    $data['catalog_menu_navigation']['region'] = 'navigation';
+    $data['catalog_menu_navigation']['weight'] = 0;
 
     $data['breadcrumbs']['region'] = 'highlighted';
     $data['breadcrumbs']['weight'] = -7;
